7532ea5369859c3775de648e9499668c0dece9b5
k7259 <sanjaykumar7259@gmail.com>
2023-04-16 10:01:13 +0530

    Add project files.

---
+++ value type/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace value_type
+{
+	 public class Class1
+	{
+		public void Changedata(int i)
+		{
+			i = 20;
+
+		}
+		public void Changedata(Student s)
+		{
+			s.i = 20;
+
+		}
+		public void Changedata(string s)
+		{
+			s = "sonu";
+
+		}
+	}
+	public class Student
+	{
+		public int i { get; set; }
+	}
+}
---
+++ value type/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using value_type;
+
+Console.WriteLine("Hello, World!");
+int i = 10;// this value type variable.
+Class1 cs = new Class1();
+cs.Changedata(i);
+Console.WriteLine(i);
+Student mystudent= new Student();// reference type variable.
+mystudent.i = 10;
+cs.Changedata(mystudent);
+Console.WriteLine(mystudent.i);
+// is string are reference type or value type ?
+// ans:=- string is a refernce type but behaviour is value type .
+string str1 = "sanjay";
+cs.Changedata(str1);
+Console.WriteLine(str1);
